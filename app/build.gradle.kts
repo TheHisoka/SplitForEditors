4459e6eaa6d418d211ed71cb5ca13971c8c65ac3
TheHisoka <bholenathjai388@gmail.com>
2026-08-08 20:56:37 +0530

    Update dependencies in build.gradle.kts
    Updated dependencies for Compose and other libraries to their latest versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,18 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("org.jetbrains.kotlin.plugin.compose") }
 android { namespace = "com.splitforeditors.app"; compileSdk = 35
  defaultConfig { applicationId = "com.splitforeditors.app"; minSdk = 29; targetSdk = 35; versionCode = 2; versionName = "2.0" }
-}
 dependencies {
- implementation("androidx.core:core-ktx:1.15.0")
- implementation("androidx.activity:activity-compose:1.10.1")
- implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
- implementation("androidx.compose.ui:ui")
- implementation("androidx.compose.ui:ui-tooling-preview")
- implementation("androidx.compose.material3:material3")
- implementation("androidx.compose.material:material-icons-extended")
- implementation("androidx.media3:media3-exoplayer:1.5.1")
- implementation("androidx.media3:media3-ui:1.5.1")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
+
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-ui:1.5.1")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
 }
